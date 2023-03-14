0ed662170b148f53ef985ad77d8353a08f0278b2
AdrianPaoletti <adriandelapao@gmail.com>
2023-03-15 00:26:01 +0100

    znz

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -21,7 +21,7 @@ export function register(config) {
     }
     window.addEventListener("load", () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
+      console.log("process.env.PUBLIC_URL", process.env.PUBLIC_URL);
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
@@ -44,6 +44,8 @@ function registerValidSW(swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
     .then((registration) => {
+      console.log("registration", registration);
+      console.log("swUrl", swUrl);
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
