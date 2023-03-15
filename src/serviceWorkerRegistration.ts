44c30814e200faf7ef26c59d7e1b1815bd05e72c
AdrianPaoletti <adriandelapao@gmail.com>
2023-03-15 18:31:25 +0100

    znz

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -22,8 +22,8 @@ export function register(config) {
       return;
     }
     window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-      console.log("process.env.PUBLIC_URL", process.env.PUBLIC_URL);
+      const swUrl = `window.location.href/service-worker.js`;
+      console.log("PROCCCCCCCC", window.location.href);
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
