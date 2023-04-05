12891a443bf64ee5d4a245d5c580a09989c3858f
AdrianPaoletti <adrian.paoletti@bideaavant.com>
2023-04-05 12:21:49 +0200

    znz

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -9,10 +9,6 @@ const Loader = () => {
         style={{
           width: "4rem",
           height: "4rem",
-          // position: "absolute",
-          // top: "50%",
-          // left: "50%",
-          // transform: "translate(-50%, -50%)",
           color: "#3c5a56",
         }}
       />
